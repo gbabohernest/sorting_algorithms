43c5be3167a278d658125a1cd6790d8961e2bcfc
Ernest <gbaboh.ernest@gmail.com>
2023-09-26 19:39:26 +0000

    cocktail_sort function

--- sort.h
+++ sort.h
@@ -27,4 +27,5 @@ int lomuto_partition(int *array, int low, int high, size_t size);
 void quicksort(int *array, int low, int high, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 #endif
